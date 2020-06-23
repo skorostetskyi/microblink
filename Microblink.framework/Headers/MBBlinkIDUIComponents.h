e898964a05e5d340425047765ed25e85ce13e557
Skorostetskyi Sergii <sergii@skorostetskyi.com>
2020-06-23 15:09:18 +0200

    5.5.0 sdk

---
+++ Microblink.framework/Headers/MBBlinkIDUIComponents.h
@@ -0,0 +1,33 @@
+//
+//  Microblink.h
+//  MicroblinkFramework
+//
+//  Created by Dino Gustin on 06/06/18.
+//  Copyright (c) 2012 Microblink Ltd. All rights reserved.
+//
+
+#import "MBBlinkInputUIComponents.h"
+#import "MBBlinkCardUIComponents.h"
+
+// Overlay delegates
+#import "MBDocumentOverlayViewControllerDelegate.h"
+#import "MBDocumentVerificationOverlayViewControllerDelegate.h"
+#import "MBBlinkIdOverlayViewControllerDelegate.h"
+//#import "MBLivenessOverlayViewControllerDelegate.h"
+
+// Overlays
+#import "MBDocumentOverlayViewController.h"
+#import "MBDocumentOverlaySettings.h"
+#import "MBDocumentVerificationOverlayViewController.h"
+#import "MBDocumentVerificationOverlaySettings.h"
+#import "MBBlinkIdOverlayViewController.h"
+#import "MBBlinkIdOverlaySettings.h"
+//#import "MBLivenessOverlayViewController.h"
+//#import "MBLivenessOverlaySettings.h"
+
+// Overlay subviews
+#import "MBDocumentSubview.h"
+#import "MBDocumentVerificationSubview.h"
+#import "MBDocumentVerificationInstructionsSubview.h"
+#import "MBCameraReticle.h"
+#import "MBErrorReticle.h"
